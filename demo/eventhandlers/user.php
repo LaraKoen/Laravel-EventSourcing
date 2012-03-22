b65968b37cb72034ffd78e84fb4ba44e3780c6d9
Koen Schmeets <k.schmeets@gmail.com>
2012-03-22 17:17:34 +0100

    Returning some information

--- demo/eventhandlers/user.php
+++ demo/eventhandlers/user.php
@@ -13,3 +13,15 @@ Bus::register('es: Demo\Events\UpdateUser', function($event) {
 Bus::register('es: Demo\Events\DeleteUser', function($event) {
 	DB::table('users')->where_uuid($event->attributes['uuid'])->delete();
 });
+
+
+/* 
+	We can return some information about this eventhandler,
+	this is handy when we are going to replay our eventlog
+	and need to know what eventhandlers should be active
+*/
+
+return array(
+	'title' => 'User Projectors',
+	'description' => 'Project user data to tables'
+);
