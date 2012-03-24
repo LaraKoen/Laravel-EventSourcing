b042b65dedae99df4e5f9f046605db394487594a
Koen Schmeets <k.schmeets@gmail.com>
2012-03-24 20:27:00 +0100

    Rename Bus method "register" to "listen" and adding some comments

--- core/bus.php
+++ core/bus.php
@@ -2,15 +2,25 @@
 
 class Bus {
 	
-	public static function register($channel, $callback)
+	/**
+	 * Listen for an event
+	 * @param  string   $channel  The event you want to listen for (prefixed with "es: ")
+	 * @param  Closure $callback The function that has to be called when the event arrives
+	 * @return Void
+	 */
+	public static function listen($channel, $callback)
 	{
 		Message::subscribe($channel, $callback);
 	}
 
+	/**
+	 * Publish an event to the listeners
+	 * @param  Event $event
+	 * @return Void
+	 */
 	public static function publish($event)
 	{
 		$identifier = get_class($event);
-		EventStore::save_events($event->attributes['uuid'], array($event));
 		Message::publish("es: {$identifier}", array($event));
 	}
 
